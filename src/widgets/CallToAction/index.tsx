a3e0db6ac1aed62c6af52ee3acf99940865a7ee3
codewithshivam <144873725+shivamcsc0909@users.noreply.github.com>
2026-02-26 22:41:43 +0530

    Update index.tsx

--- src/widgets/CallToAction/index.tsx
+++ src/widgets/CallToAction/index.tsx
@@ -30,7 +30,7 @@ const Index: FC<Props> = () => {
       </div>
 
       <footer className="flex justify-between border-t border-t-gray-800 px-[5vw] py-[1.8vw] text-[1.6vw] md:text-[2vw] md:py-[2.4vw] md:px-[2vw] ">
-        <div>© 2023. <a href="https://github.com/Shatlyk1011/agency-website" target='_blank' rel="norelopener">Source Code</a></div>
+        <div>© 2023. <a href="https://github.com/shivamcsc0909/Smooth-fluid-animation-marketing-website" target='_blank' rel="norelopener">Source Code</a></div>
         <ul className="flex space-x-[3vw] ">
           <li>Lorem, ipsum.</li>
           <li>Lorem, ipsum.</li>
@@ -41,3 +41,4 @@ const Index: FC<Props> = () => {
   );
 };
 export default Index;
+
